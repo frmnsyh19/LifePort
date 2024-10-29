7055ad60a5681108ea9b792c3b38e7958b6fd915
frmnsyh19 <19.firmann@gmail.com>
2024-10-29 13:18:00 +0700

    newcodecommit3

--- app/components/sportComponent/Title.tsx
+++ app/components/sportComponent/Title.tsx
@@ -5,7 +5,7 @@ export const Title = () => {
   return (
     <>
       <div className=' w-full flex justify-center items-center'>
-        <div className="w-[90%]  flex=col flex justify-center items-center p-3">
+        <div className=" w-full lg:w-[90%] flex=col flex justify-center items-center p-3">
           <div className=" w-[35rem] flex-col gap-5 flex justify-center p-2 items-center ">
             <div className=' w-16 p-1 flex-col gap-2 rounded-md bg-slate-200 flex justify-center items-center'>
               <p className=' text-base text-gray-400'>Healthy</p>
@@ -14,7 +14,17 @@ export const Title = () => {
               Posisi Duduk yang Tepat untuk Mencegah
               Gangguan Otot, Tulang dan Mata Saat Bekerja
             </span>
-            <div className=' flex flex-col lg:flex-row'>
+            <div className=' flex w-full flex-col items-center justify-center lg:flex-row'>
+              <div className=' p-2 flex flex-row items-center gap-1'>
+                <FiUser className=' text-sm lg:text-base text-gray-400' />
+                <p className=' text-sm lg:text-base  text-gray-400'>Andika Chris Ardiansyah - 2371510690,</p>
+              </div>
+              <div className=' p-2 flex flex-row items-center gap-1'>
+                <FiCalendar className=' text-sm lg:text-base text-gray-400' />
+                <p className=' text-sm lg:text-base text-gray-400'>26 Oktober 2024</p>
+              </div>
+            </div>
+            {/* <div className=' flex flex-col justify-center text-center lg:flex-row'>
               <div className=' p-2 flex flex-row gap-1'>
                 <FiUser className=' text-xl text-gray-400' />
                 <p className=' text-gray-400'>Andika Chris Ardiansyah - 2371510690,</p>
@@ -23,7 +33,7 @@ export const Title = () => {
                 <FiCalendar className=' text-xl text-gray-400' />
                 <p className=' text-gray-400'>26 Oktober 2024</p>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
