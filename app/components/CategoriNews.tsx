c0ba0bb815dd6f4d79f72deda1fce53660897954
frmnsyh19 <19.firmann@gmail.com>
2024-10-30 15:50:23 +0700

    the final result

--- app/components/CategoriNews.tsx
+++ app/components/CategoriNews.tsx
@@ -1,46 +1,57 @@
-import React from 'react'
-
+import React from "react";
 
 export const CategoriNews = () => {
   return (
     <>
       <div className="w-full gap-2 flex flex-col p-2">
-        <div className=' w-full p-3 border-b border-slate-400'>
-          <span className=' text-slate-500 text-lg'>Categories</span>
+        <div className=" w-full p-3 border-b border-slate-400">
+          <span className=" text-slate-500 text-lg">
+            Categories
+          </span>
         </div>
-        <div className=' w-full p-2 flex flex-col gap-1'>
-          <div className=' w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2'>
-            <span className=' text-base text-neutral'>Heathly</span>
-            <span className=' mr-1 text-end'>
+        <div className=" w-full p-2 flex flex-col gap-1">
+          <div className=" w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2">
+            <span className=" text-base text-neutral">
+              Heathly
+            </span>
+            <span className=" mr-1 text-end">
               1
             </span>
           </div>
-          <div className=' w-full h-12 items-center bg-slate-200 gap-1 hover:bg-slate-300 justify-between flex p-2'>
-            <span className=' text-base text-neutral'>Sport</span>
-            <span className=' mr-1 text-end'>
+          <div className=" w-full h-12 items-center bg-slate-200 gap-1 hover:bg-slate-300 justify-between flex p-2">
+            <span className=" text-base text-neutral">
+              Sport
+            </span>
+            <span className=" mr-1 text-end">
               2
             </span>
           </div>
-          <div className=' w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2'>
-            <span className=' text-base text-neutral'>Food</span>
-            <span className=' mr-1 text-end'>
+          <div className=" w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2">
+            <span className=" text-base text-neutral">
+              Food
+            </span>
+            <span className=" mr-1 text-end">
               2
             </span>
           </div>
-          <div className=' w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2'>
-            <span className=' text-base text-neutral'>Fashion</span>
-            <span className=' mr-1 text-end'>
+          <div className=" w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2">
+            <span className=" text-base text-neutral">
+              Fashion
+            </span>
+            <span className=" mr-1 text-end">
               1
             </span>
           </div>
-          <div className=' w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2'>
-            <span className=' text-base text-neutral'>Heathly</span>
-            <span className=' mr-1 text-end'>
+          <div className=" w-full h-12 items-center bg-slate-200 hover:bg-slate-300 justify-between flex p-2">
+            <span className=" text-base text-neutral">
+              Technology
+            </span>
+            <span className=" mr-1 text-end">
               1
             </span>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
