12fa07897df8a6fda1797911630cb08d34145005
frmnsyh19 <19.firmann@gmail.com>
2024-10-30 15:46:24 +0700

    final result

--- app/components/NavbarMobile.tsx
+++ app/components/NavbarMobile.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import Dawer from './Dawer'
-import Image from 'next/image'
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import Dawer from "./Dawer";
+import Image from "next/image";
 
 const NavbarMobile = () => {
-
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] =
+    useState(false);
 
   // Scroll event listener
   useEffect(() => {
@@ -18,23 +21,60 @@ const NavbarMobile = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener(
+      "scroll",
+      handleScroll
+    );
+    return () =>
+      window.removeEventListener(
+        "scroll",
+        handleScroll
+      );
   }, []);
-
 
   return (
     <>
-      <div className={`fixed navbar p-2 lg:hidden flex justify-between items-center top-0 left-0 w-full z-50 transition-all duration-300 
-      ${isScrolled ? 'bg-indigo-900 shadow-lg' : 'bg-transparent'
-        }`}>
-        <Dawer />
-        <div className=' p-2'>
-          <Image src='/image/logo.png' width={50} height={50} alt='' />
+      <div
+        className={`fixed navbar p-2 lg:hidden flex justify-between items-center top-0 left-0 w-full z-50 transition-all duration-300 
+      ${
+        isScrolled
+          ? "bg-indigo-900 shadow-lg"
+          : "bg-transparent"
+      }`}>
+        <div className=" p-1">
+          <Dawer />
         </div>
+        {isScrolled ? (
+          <>
+            <div className="p-1">
+              <Image
+                width={150}
+                height={150}
+                alt="logo lifeport"
+                src="/image/imgmobile.png"
+              />
+            </div>
+          </>
+        ) : (
+          ""
+        )}
+        {isScrolled ? (
+          <>
+            <div className=" p-2">
+              <Image
+                src="/image/logo.png"
+                width={50}
+                height={50}
+                alt=""
+              />
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default NavbarMobile
+export default NavbarMobile;
--- app/components/sportscomponent/SomethingFresh2.tsx
+++ app/components/sportscomponent/SomethingFresh2.tsx
@@ -1,46 +1,60 @@
-import React from 'react'
-import { FiCalendar } from 'react-icons/fi'
-import { CategoriNews } from '../CategoriNews'
+import React from "react";
+import { FiCalendar } from "react-icons/fi";
+import { CategoriNews } from "../CategoriNews";
 
 export const SomethingFresh2 = () => {
   return (
     <>
       <div className=" w-full lg:w-[35%] p-2 flex flex-col justify-start gap-2">
         {/* something fresh */}
-        <div className=' flex w-full p-3 flex-col justify-start'>
+        <div className=" flex w-full p-3 flex-col justify-start">
           {/* title */}
-          <div className='p-2 w-full border-gray-400 border-b '>
-            <span className=' text-slate-500 text-xl font-semibold capitalize'>
+          <div className="p-2 w-full border-gray-400 border-b ">
+            <span className=" text-slate-500 text-xl font-semibold capitalize">
               Something Fresh
             </span>
           </div>
-          <div className=' w-full flex flex-col justify-start items-center'>
+          <div className=" w-full flex flex-col justify-start items-center">
             <div className="w-full flex flex-row justify-start items-center gap-2 p-2">
-              <div className=' w-48 h-20'>
-                <img src="/image/newssports2.png" className='w-full h-full' alt="" />
+              <div className=" w-48 h-20">
+                <img
+                  src="/image/newssports2.png"
+                  className="w-full h-full"
+                  alt=""
+                />
               </div>
-              <div className=' w-full flex flex-col p-1 gap-1 justify-start'>
-                <span className=' text-base text-indigo-800'>
-                  bersepeda adalah olahraga kardio yang lebih bagus
-                  dari pada lari apakah benar?
+              <div className=" w-full flex flex-col p-1 gap-1 justify-start">
+                <span className=" text-base text-indigo-800 capitalize">
+                  bersepeda adalah olahraga kardio
+                  yang lebih bagus dari pada lari
+                  apakah benar?
                 </span>
-                <div className=' flex w-full flex-row justify-start items-center gap-1 '>
-                  <FiCalendar className=' text-base text-gray-400' />
-                  <span className=' text-base text-gray-400'>26 Oktober 2024</span>
+                <div className=" flex w-full flex-row justify-start items-center gap-1 ">
+                  <FiCalendar className=" text-base text-gray-400" />
+                  <span className=" text-base text-gray-400">
+                    26 Oktober 2024
+                  </span>
                 </div>
               </div>
             </div>
             <div className="w-full flex flex-row justify-start items-center gap-2 p-2">
-              <div className=' w-48 h-20'>
-                <img src="/image/headlinenews4.jpg" className='w-full h-full' alt="" />
+              <div className=" w-48 h-20">
+                <img
+                  src="/image/headlinenews4.jpg"
+                  className="w-full h-full"
+                  alt=""
+                />
               </div>
-              <div className=' w-full flex flex-col p-1 gap-1 justify-start'>
-                <span className=' text-base text-indigo-800'>
-                  Jogging Malam, Tren Sehat Baru di Kalangan Pekerja Muda
+              <div className=" w-full flex flex-col p-1 gap-1 justify-start">
+                <span className=" text-base text-indigo-800">
+                  Jogging Malam, Tren Sehat Baru
+                  di Kalangan Pekerja Muda
                 </span>
-                <div className=' flex w-full flex-row justify-start items-center gap-1 '>
-                  <FiCalendar className=' text-base text-gray-400' />
-                  <span className=' text-base text-gray-400'>26 Oktober 2024</span>
+                <div className=" flex w-full flex-row justify-start items-center gap-1 ">
+                  <FiCalendar className=" text-base text-gray-400" />
+                  <span className=" text-base text-gray-400">
+                    26 Oktober 2024
+                  </span>
                 </div>
               </div>
             </div>
@@ -50,5 +64,5 @@ export const SomethingFresh2 = () => {
         <CategoriNews />
       </div>
     </>
-  )
-}
+  );
+};
