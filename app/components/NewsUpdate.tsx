c0914b4ef3e5d6fedceefab35b7db551e04e9e98
frmnsyh19 <19.firmann@gmail.com>
2024-10-31 10:50:56 +0700

    result

--- app/components/NewsUpdate.tsx
+++ app/components/NewsUpdate.tsx
@@ -1,12 +1,12 @@
-"use client"
-import '../style.css'
-import React from 'react'
-import { FiCalendar } from 'react-icons/fi'
-import Button from '@mui/material/Button'
-import { useEffect } from 'react'
-import aos from 'aos'
+"use client";
+import "../style.css";
+import React from "react";
+import { FiCalendar } from "react-icons/fi";
+import Button from "@mui/material/Button";
+import { useEffect } from "react";
+import aos from "aos";
 import "aos/dist/aos.css";
-import Link from 'next/link'
+import Link from "next/link";
 export const NewsUpdate = () => {
   useEffect(() => {
     aos.init({
@@ -16,126 +16,187 @@ export const NewsUpdate = () => {
   }, []);
   return (
     <>
-      <div data-aos="fade-down" className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
-        <div className=' w-full lg:w-[40rem] h-72' id='newshealthy'></div>
-        <div className=' w-full justify-between gap-5 flex flex-col p-2'>
-          <span className=' text-xl text-slate-400 font-bold'>
-            {'"Posisi Duduk yang Tepat untuk Mencegah Gangguan Otot,Tulang dan Mata Saat Bekerja"'}
+      <div
+        data-aos="fade-down"
+        className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
+        <div
+          className=" w-full lg:w-[40rem] h-72"
+          id="newshealthy"></div>
+        <div className=" w-full justify-between gap-5 flex flex-col p-2">
+          <span className=" text-xl text-slate-400 font-bold">
+            {
+              '"Posisi Duduk yang Tepat untuk Mencegah Gangguan Otot,Tulang dan Mata Saat Bekerja"'
+            }
           </span>
-          <div className=' w-full gap-1 flex flex-row items-center'>
-            <FiCalendar className=' text-base' />
-            <span className=' text-base'>
+          <div className=" w-full gap-1 flex flex-row items-center">
+            <FiCalendar className=" text-base" />
+            <span className=" text-base">
               26 Oktober 2024
             </span>
           </div>
-          <span className=' text-slate-400 text-base'>
-            Bagi banyak pekerja kantoran, duduk di depan komputer selama berjam-jam sudah
+          <span className=" text-slate-400 text-base">
+            Bagi banyak pekerja kantoran, duduk di
+            depan komputer selama berjam-jam sudah
             menjadi rutinitas sehari-hari.
           </span>
-          <div className=' w-40'>
-            <Link href='/healthy'>
-              <Button variant="contained" size='large' className=' bg-indigo-700 text-white hover:bg-indigo-900'>
-                Read More...
-              </Button></Link>
-          </div>
-        </div>
-      </div>
-      <div data-aos="fade-down" className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
-        <div className=' w-full lg:w-[40rem]  h-72' id='news2'></div>
-        <div className=' w-full justify-between gap-5 flex flex-col p-2'>
-          <span className=' text-xl text-slate-400 font-bold'>
-            {'" Nasi beku bagus untuk pola makan dan konsumsi masyarakat."'}
-          </span>
-          <div className=' w-full gap-1 flex flex-row items-center'>
-            <FiCalendar className=' text-base' />
-            <span className=' text-base'>
-              26 Oktober 2024
-            </span>
-          </div>
-          <span className=' text-slate-400 text-base'>
-            Dalam rutinitas yang padat, nasi
-            beku jadi cara cepat untuk menyajikan makanan tanpa proses memasak yang
-            panjang. Hal ini sangat relevan di zaman sekarang
-          </span>
-          <div className=' w-40'>
-            <Link href='/food'>
-              <Button variant="contained" size='large' className=' bg-indigo-700 text-white hover:bg-indigo-900'>
+          <div className=" w-40">
+            <Link href="/healthy">
+              <Button
+                variant="contained"
+                size="large"
+                className=" bg-indigo-700 text-white hover:bg-indigo-900">
                 Read More...
               </Button>
             </Link>
           </div>
         </div>
       </div>
-      <div data-aos="fade-down" className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
-        <div className=' w-full lg:w-[40rem] h-72' id='news3'></div>
-        <div className=' w-full justify-between gap-5 flex flex-col p-2'>
-          <span className=' text-xl text-slate-400 font-bold'>
-            {'"Kenapa Saat Ini Nail Art Menjadi Salah Satu Fashion Yang Menjadi Hal Wajib Untuk Wanita?"'}
+      <div
+        data-aos="fade-down"
+        className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
+        <div
+          className=" w-full lg:w-[40rem]  h-72"
+          id="news2"></div>
+        <div className=" w-full justify-between gap-5 flex flex-col p-2">
+          <span className=" text-xl text-slate-400 font-bold">
+            {
+              '" Nasi beku bagus untuk pola makan dan konsumsi masyarakat."'
+            }
           </span>
-          <div className=' w-full gap-1 flex flex-row items-center'>
-            <FiCalendar className=' text-base' />
-            <span className=' text-base'>
+          <div className=" w-full gap-1 flex flex-row items-center">
+            <FiCalendar className=" text-base" />
+            <span className=" text-base">
               26 Oktober 2024
             </span>
           </div>
-          <span className=' text-slate-400 text-base'>
-            Nail art identik dengan perempuan yang ingin mempercantik tampilan kuku mereka. Seni tersebut semakin banyak digandrungi remaja saat ini.
+          <span className=" text-slate-400 text-base">
+            Dalam rutinitas yang padat, nasi beku
+            jadi cara cepat untuk menyajikan
+            makanan tanpa proses memasak yang
+            panjang. Hal ini sangat relevan di
+            zaman sekarang
           </span>
-          <div className=' w-40'>
-            <Link href='/fashion'>
-              <Button variant="contained" size='large' className=' bg-indigo-700 text-white hover:bg-indigo-900'>
+          <div className=" w-40">
+            <Link href="/food">
+              <Button
+                variant="contained"
+                size="large"
+                className=" bg-indigo-700 text-white hover:bg-indigo-900">
                 Read More...
               </Button>
             </Link>
           </div>
         </div>
       </div>
-      <div data-aos="fade-down" className="w-full lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
-        <div className=' w-full lg:w-[40rem] h-72' id='news4'></div>
-        <div className=' w-full justify-between gap-5 flex flex-col p-2'>
-          <span className=' capitalize text-xl text-slate-400 font-bold'>
-            {'"bersepeda adalah olahraga kardio yang lebih bagus dari pada lari apakah benar? kesehatan yang tak kalah penting dari olahraga pagi."'}
+      <div
+        data-aos="fade-down"
+        className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
+        <div
+          className=" w-full lg:w-[40rem] h-72"
+          id="news3"></div>
+        <div className=" w-full justify-between gap-5 flex flex-col p-2">
+          <span className=" text-xl text-slate-400 font-bold">
+            {
+              '"Kenapa Saat Ini Nail Art Menjadi Salah Satu Fashion Yang Menjadi Hal Wajib Untuk Wanita?"'
+            }
           </span>
-          <div className=' w-full gap-1 flex flex-row items-center'>
-            <FiCalendar className=' text-base' />
-            <span className=' text-base'>
+          <div className=" w-full gap-1 flex flex-row items-center">
+            <FiCalendar className=" text-base" />
+            <span className=" text-base">
               26 Oktober 2024
             </span>
           </div>
-          <span className=' text-slate-400 text-base'>
-            Di tengah tren gaya hidup sehat, bersepeda semakin populer sebagai pilihan olahraga kardio,
-            terutama bagi kalangan muda yang berusaha menjaga kesehatan
+          <span className=" text-slate-400 text-base">
+            Nail art identik dengan perempuan yang
+            ingin mempercantik tampilan kuku
+            mereka. Seni tersebut semakin banyak
+            digandrungi remaja saat ini.
           </span>
-          <div className=' w-40'>
-            <Link href='/sport'>
-              <Button variant="contained" size='large' className=' bg-indigo-700 text-white hover:bg-indigo-900'>
+          <div className=" w-40">
+            <Link href="/fashion">
+              <Button
+                variant="contained"
+                size="large"
+                className=" bg-indigo-700 text-white hover:bg-indigo-900">
                 Read More...
               </Button>
             </Link>
           </div>
         </div>
       </div>
-      <div data-aos="fade-down" className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
-        <div className=' w-full lg:w-[40rem] h-72' id='news5'></div>
-        <div className=' w-full justify-between gap-5 flex flex-col p-2'>
-          <span className=' text-xl text-slate-400 font-bold'>
-            {'"Smartwatch: Sahabat Digital Gen Z untuk Monitoring Kesehatan Sehari-hari"'}
+      <div
+        data-aos="fade-down"
+        className="w-full lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
+        <div
+          className=" w-full lg:w-[40rem] h-72"
+          id="news4"></div>
+        <div className=" w-full justify-between gap-5 flex flex-col p-2">
+          <span className=" capitalize text-xl text-slate-400 font-bold">
+            {
+              '"bersepeda adalah olahraga kardio yang lebih bagus dari pada lari apakah benar?"'
+            }
           </span>
-          <div className=' w-full gap-1 flex flex-row items-center'>
-            <FiCalendar className=' text-base' />
-            <span className=' text-base'>
+          <div className=" w-full gap-1 flex flex-row items-center">
+            <FiCalendar className=" text-base" />
+            <span className=" text-base">
               26 Oktober 2024
             </span>
           </div>
-          <span className=' text-slate-400 text-base'>
-            Bagi banyak Gen Z, teknologi sudah menjadi bagian tak terpisahkan dari kehidupan
-            sehari-hari. Salah satu perangkat yang semakin populer di kalangan mereka adalah
-            smartwatch. Lebih dari sekadar aksesori gaya, smartwatch kini menjadi alat penting
-            untuk memantau kesehatan dan kebugaran, terutama bagi mereka yang menjalani rutinitas kerja padat.
+          <span className=" text-slate-400 text-base">
+            Di tengah tren gaya hidup sehat,
+            bersepeda semakin populer sebagai
+            pilihan olahraga kardio, terutama bagi
+            kalangan muda yang berusaha menjaga
+            kesehatan
           </span>
-          <div className=' w-40'>
-            <Link href='/technology'>
-              <Button variant="contained" size='large' className=' bg-indigo-700 text-white hover:bg-indigo-900'>
+          <div className=" w-40">
+            <Link href="/sport">
+              <Button
+                variant="contained"
+                size="large"
+                className=" bg-indigo-700 text-white hover:bg-indigo-900">
+                Read More...
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div
+        data-aos="fade-down"
+        className="w-full  lg:w-[95%] gap-2 flex-col justify-start items-center flex  lg:flex-row">
+        <div
+          className=" w-full lg:w-[40rem] h-72"
+          id="news5"></div>
+        <div className=" w-full justify-between gap-5 flex flex-col p-2">
+          <span className=" text-xl text-slate-400 font-bold">
+            {
+              '"Smartwatch: Sahabat Digital Gen Z untuk Monitoring Kesehatan Sehari-hari"'
+            }
+          </span>
+          <div className=" w-full gap-1 flex flex-row items-center">
+            <FiCalendar className=" text-base" />
+            <span className=" text-base">
+              26 Oktober 2024
+            </span>
+          </div>
+          <span className=" text-slate-400 text-base">
+            Bagi banyak Gen Z, teknologi sudah
+            menjadi bagian tak terpisahkan dari
+            kehidupan sehari-hari. Salah satu
+            perangkat yang semakin populer di
+            kalangan mereka adalah smartwatch.
+            Lebih dari sekadar aksesori gaya,
+            smartwatch kini menjadi alat penting
+            untuk memantau kesehatan dan
+            kebugaran, terutama bagi mereka yang
+            menjalani rutinitas kerja padat.
+          </span>
+          <div className=" w-40">
+            <Link href="/technology">
+              <Button
+                variant="contained"
+                size="large"
+                className=" bg-indigo-700 text-white hover:bg-indigo-900">
                 Read More...
               </Button>
             </Link>
@@ -143,5 +204,5 @@ export const NewsUpdate = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const Swipers = React.lazy(
 export default function Home() {
   return (
     <>
-      <div className=" w-full flex flex-col bg-slate-100 text-neutral">
+      <div className=" w-full flex flex-col bg-slate-100 text-neutral2371510849">
         <NavbarPc />
         <NavbarMobile />
 
@@ -24,8 +24,15 @@ export default function Home() {
         <Headline />
         <div className=" mt-4 lg:mt-20 w-full justify-center h-full items-center flex gap-2">
           <div className="w-full lg:w-[90%] flex flex-col lg:flex-row gap-2 p-3">
-            <div className="w-full lg:w-[70%] flex-col justify-center items-center flex gap-7 ">
-              <NewsUpdate />
+            <div className="w-full lg:w-[70%] flex-col justify-center items-center flex gap-2 lg:gap-7 ">
+              <div className=" w-full flex justify-start items-center p-2 lg:p-3 border-b border-b-gray-500">
+                <span className=" text-slate-400 font-bold text-2xl lg:text-3xl">
+                  News Update
+                </span>
+              </div>
+              <div className="w-full p-1 flex-col justify-center items-center flex gap-7">
+                <NewsUpdate />
+              </div>
             </div>
             <div className=" w-full lg:w-[35%] gap-3 p-1">
               <CategoriNews />
